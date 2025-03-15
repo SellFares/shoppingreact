415a12b7bdcc4ddda1572835005c0f48bc96c4f0
Ralph Valentin <Ralphvalentin05@gmail.com>
2025-03-15 14:40:30 -0500

    RV Version

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer,  // cart is the key and cartReducer is the value
     },
 });
 export default store
