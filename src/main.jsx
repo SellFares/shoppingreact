fc602c6ced30181310d601a3cf17e3b0ca864e13
InterstellarGalaxy <94878436+InterstellarGalaxy@users.noreply.github.com>
2025-03-28 00:27:43 +0100

    Implement ProductList component with Redux integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
-)
+  </Provider>
+);
