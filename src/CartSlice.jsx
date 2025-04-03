a7158036a10385abd1c19ff2a89e2b3c84a5e71e
“StephanieR20” <sr_1289@yahoo.com>
2025-04-02 23:54:18 -0400

    Your descriptive commit message here

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -21,13 +21,10 @@ export const CartSlice = createSlice({
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-          itemToUpdate.quantity = quantity++;
-       
-        }
-   
-        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
 
     },
   },
