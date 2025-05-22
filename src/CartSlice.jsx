026879c7ca2d855d356c346c636987e552dbd8e1
RonManatee <athompson0103@gmail.com>
2025-05-22 08:58:31 -0400

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -11,34 +11,25 @@ export const CartSlice = createSlice({
   const { name, image, cost } = action.payload; 
   const existingItem = state.items.find(item => item.name === name);
   if (existingItem) {
-    
     existingItem.quantity++;
   } else {
-   
-    state.items.push({ name, image, cost, quantity: 1 });
-    
-    const handleAddToCart = (product) => {
-      dispatch(addItem(product));
-
-      SetAddedToCart((prevState) => ({
-        ...prevState, 
-        [product.name]: true,
-      }));
-    };
-    }
-  },
- },
-  },
-    
+   state.items.push({ name, image, cost, quantity: 1 });
+  }
+},
     
     removeItem: (state, action) => {
-      state.items - state.items.filter(item => !== action.payload);
-    },
+        const item=action.payload;
+        const existingItem=State.items.find((i)=>i.name===item.name);
+        if(existingItem){
+      state.items=state.items.filter((i)=>i.name!==item.name);
+    }
+  },
     updateQuantity: (state, action) => {
       const { name, image, cost } = action.payload;
-          const itemToUpdate = state.items.find(item => item.name === name);
-            if (itemToUpdate) {
-              itemToUpdate.quantity = quantity; 
+          const item = state.items.find((i) => i.name === name);
+          item.quantity= quantity;
+            if (item.quantity === 0) {
+              state.items = state.items.filter((i) => i.name !==name); 
         }
     
     },
