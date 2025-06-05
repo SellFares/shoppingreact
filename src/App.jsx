29ede75acde5e8ee9a1df42081591461eb38b323
Alex Kagunda <81379641+Alex-kagunda@users.noreply.github.com>
2025-06-05 14:10:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-
-import React, { useState } from 'react';
+// src/App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import ProductList from './ProductList';
+import CartItem from './CartItem';
+import AboutUs from './AboutUs'; // This is our LandingPage
 import './App.css';
-import AboutUs from './AboutUs';
 
 function App() {
-  
-  const [showProductList, setShowProductList] = useState(false);
-
-  const handleGetStartedClick = () => {
-    setShowProductList(true);
-  };
-
-  const handleHomeClick = () => {
-    setShowProductList(false);
-  };
-
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
-      </div>
+    <div>
+      {/* No global header here, headers are inside ProductList and CartItem as per current structure */}
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/products" element={<ProductList />} /> {/* Changed from /productlist */}
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
