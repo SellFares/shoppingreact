406d791e0d44fe6fa319c32d74a8f8c6dc7e25fa
Alex Kagunda <81379641+Alex-kagunda@users.noreply.github.com>
2025-06-05 14:22:28 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Your global CSS file, if you have one
+import './index.css'; // Your global base styles (ensure this file exists or remove if not used)
 
 // Import Provider from react-redux to make the store available to your app
 import { Provider } from 'react-redux';
 // Import your configured Redux store
-import store from './store.js'; // This assumes store.js is in the src/ directory
+import store from './store.js'; // Assumes store.js is in the src/ directory
 
 // Import BrowserRouter for routing capabilities
 import { BrowserRouter } from 'react-router-dom';
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* BrowserRouter should wrap components that need routing capabilities */}
     <BrowserRouter>
-      {/* Provider makes the Redux store available to any nested components that have been wrapped in connect() or use useSelector/useDispatch */}
+      {/* Provider makes the Redux store available to any nested components */}
       <Provider store={store}>
         <App />
       </Provider>
