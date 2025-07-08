56f4f8c6028377eaa2a378910ebb179f66c02d20
vmosharova <varya.mosharova@gmail.com>
2025-07-08 14:10:46 +0200

    Feat: update reducers in CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      state.push(action.payload);
+      const { name, image, description, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({name, image, description, cost, quantity: 1});        
+      }
     },
     removeItem: (state, action) => {
+        const { name } = action.payload;
+        state.items = state.items.filter(item => item.name != name);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        existingItem.quantity = quantity; 
     },
   },
 });
