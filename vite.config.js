be1d03a78ac4de77075d7ea26e4f070bf3b85bd6
Eli-B978 <hellokinpoboy04@outlook.com>
2025-07-24 04:27:01 -0400

    feat: base Repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
+  base: "/Eli-B978/e-plantShopping",
   plugins: [react()],
 })
