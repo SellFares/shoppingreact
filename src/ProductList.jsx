5a879a358547220317ed1097f021ae933134c229
Eli-B978 <hellokinpoboy04@outlook.com>
2025-07-24 03:25:33 -0400

    feat: Calculation of Total Quantity

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -295,6 +295,11 @@ function ProductList({ onHomeClick }) {
         }));
 
     const cartItems = useSelector((state) => state.cart.items);
+
+    const calculateTotalQuantity = () => {
+        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
+
     };
 
     return (
@@ -322,6 +327,7 @@ function ProductList({ onHomeClick }) {
                                     <circle cx="184" cy="216" r="12"></circle>
                                     <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
                                 </svg>
+                                <span>({calculateTotalQuantity()})</span>
                             </h1>
                         </a>
                     </div>
