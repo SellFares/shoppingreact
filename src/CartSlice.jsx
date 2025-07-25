a6f8103c47100b50a8ff0bf1e16064fe31591acd
Eli-B978 <hellokinpoboy04@outlook.com>
2025-07-24 23:29:02 -0400

    feat: creation of addItem, removeItem, updateQuantity in CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+
+const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -7,13 +14,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost} = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
