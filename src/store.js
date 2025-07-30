72cf29f125687039c7de3410cf6d1d323e2ddcc8
Marcos Mora <abreu760@hotmail.com>
2025-07-30 17:19:24 -0400

    update cartslice

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+import cartReducer, { addItem, removeItem, updateQuantity } from './CartSlice';
  const store = configureStore({
     reducer: {
         cart: cartReducer,
+        addItem:
+        removeItem:
+        updateQuantity:
     },
 });
 export default store
