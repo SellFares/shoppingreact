10b98a2b5bce87aa0c15209500f82f893affbf59
Marcos Mora <abreu760@hotmail.com>
2025-08-02 02:23:07 -0400

    update cartslice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -9,14 +9,17 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
         const { name, image, cost } = action.payload; // Destructure product details from the action payload
+
         // Check if the item already exists in the cart by comparing names
         const existingItem = state.items.find(item => plant.name === name);
         if (existingItem) {
+            dispatch(addItem({ name: item.name, quantity: item.quantity + 1 }));
           // If item already exists in the cart, increase its quantity
           existingItem.quantity++;
         } else {
           // If item does not exist, add it to the cart with quantity 1
           state.items.push({ name, image, cost, quantity: 1 });
+          dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
         } 
     
     },
@@ -35,17 +38,19 @@ export const CartSlice = createSlice({
                     .map(item => 
                         item.name === name
                             ? { ...item, quantity } // Crea una copia del item actualizando solo la cantidad
-                            : item
+                            (
+                            dispatch(updateQuantity({ name: item.name, quantity: item.quantity =0 }));
                     )
-                    .filter(item => item.quantity > 0) // Elimina items con cantidad <= 0
-            };
-        }
-}
+                    
+                
+            },
+        
+        };
 
 
     
-    },
-  },
+    }
+  }
 );
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
