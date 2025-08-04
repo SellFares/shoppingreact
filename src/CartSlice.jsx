493d4aba72343fdb064c097962bb8fbd21c475d9
Marcos Mora <abreu760@hotmail.com>
2025-08-03 22:09:55 -0400

    modified:   src/CartSlice.jsx         modified:   src/ProductList.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,16 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+          const existingItem = state.items.find(item => item.id === action.payload.id);
+            
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.items.push(action.payload);
+            }
+        }
     
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+// Find the item in the cart that matches the given name
+const itemToUpdate = state.items.find(item => item.name === name);
+if (itemToUpdate) {
+  itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+}
 
     
     },
   },
-});
+);
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
