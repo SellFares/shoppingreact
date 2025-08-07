a115afce30b426e632d44abc09ceea61d8298e53
Mitali Brahmankar <mitali.brahmankar23@pccoepune.org>
2025-08-07 13:10:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/e-plantShopping/", // GitHub repo name
+  base: process.env.NODE_ENV === "production" ? "/e-plantShopping/" : "/",
   plugins: [react()],
 });
-
