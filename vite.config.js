27e9f2f137f4204d9724aa729221cc2550671e36
aishakhanjadoon <aishaxjadoonxa@gmail.com>
2025-08-12 02:46:46 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  // This 'base' path must match your repository name exactly.
+  base: '/e-plantShopping/',
   plugins: [react()],
 })
