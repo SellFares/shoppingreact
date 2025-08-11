e1c6853752e30a9705f9a2b7b89dd1f3b105a9cd
aishakhanjadoon <aishaxjadoonxa@gmail.com>
2025-08-12 02:28:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ProductList from './ProductList';
-import './App.css'; // Make sure to create this CSS file
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
+      {/* ProductList is the root component that handles both product view and cart view */}
       <ProductList />
     </div>
   );
