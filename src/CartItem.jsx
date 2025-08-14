ef36183a91b7113c3026460271bc77c7e60683b0
Kur0Shir0 <mihassssssssik64@gmail.com>
2025-08-14 11:07:44 -0400

    Smth added

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,32 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return items ? items.reduce((acc, item) => acc + (item.quantity * parseFloat(item.cost.substring(1))), 0) : 0;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name : item.name, quantity: ++item.quntity}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+        dispatch(updateQuantity({name : item.name, quantity: --item.quntity}));
+    }
+    else if(item.quantity === 1){
+        dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
-
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1))
   };
 
   return (
