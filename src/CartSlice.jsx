c25599aed3815dbbf39d7f59bb4f64f5ccd1b19e
rodrigcasio <rod.casio@outlook.com>
2025-09-10 16:36:36 -0600

    Added functionality for function reducer "addItems()"

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,15 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;       // destructive product details from the action payload
+
+      const existingItem = state.items.find(item => item.name === name);    // chcking if the item already exists in the cart  by comparing names
+      if(existingItem){
+        existingItem.quantity++;    // if item already exists in the cart, increase its quantity
+      }else{
+        state.items.push({ name, image, cost, quantity: 1 });         // if item does not exist, add it to the cart with quantity 1
+      }
+      
     },
     removeItem: (state, action) => {
     },
