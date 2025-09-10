0946181653c2a6b68ee67f0e89fcbbb78bd208b0
rodrigcasio <rod.casio@outlook.com>
2025-09-10 16:53:23 -0600

    Created the logic for the reducer functions: "removeItem and updateQuantity"

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -6,6 +6,8 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
+    
+    // reducer adds a new plant item to the "items" array 
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;       // destructive product details from the action payload
 
@@ -16,10 +18,20 @@ export const CartSlice = createSlice({
         state.items.push({ name, image, cost, quantity: 1 });         // if item does not exist, add it to the cart with quantity 1
       }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
+    // reducer removes an item from the cart based on its name and gets called when the user wants to remove products from the cart
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;    // Destructive the product name and new quantity from the action payload 
+
+      // *find the item in the cart that matches the given name
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if(itemToUpdate){
+        itemToUpdate.quantity = quantity;       // if item is found, update its quantity to the new value
+      }
     
     },
   },
