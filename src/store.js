108a774fdabb49ab1bba0d297a8e0a70ac58e657
Fan Gao <fancrazy88@gmail.com>
2025-09-12 14:03:53 -0600

    task 5 is done

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
  const store = configureStore({
+    // Define the root reducer object
     reducer: {
         cart: cartReducer,
     },
